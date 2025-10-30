ebd237d30615c202b440884bdacdb779f74420b5
Marco Nätlitz <432346+marconae@users.noreply.github.com>
2025-10-30 10:07:48 +0100

    Introduced setup.sh - one stop script to install dependencies: Ghostty. Atuin, Meslo LG Nerd Font, Powerlevel10k

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Ghostty
+brew install --cask ghostty
+
+# atuin
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
+# Meslo LG Nerd Font
+brew install font-meslo-lg-nerd-font
+
+# powerlevel10k theme for ZSH
+brew install powerlevel10k
